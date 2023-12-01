3360cddae598ba1c93203483dec46f6161c69fb2
Stiven <delleore1@gmail.com>
2023-11-30 21:10:06 -0500

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   desktop.ini 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/img/icons/Android OS.png 	new file:   public/img/icons/JavaScript.png 	new file:   public/img/icons/Python.png 	new file:   public/img/icons/React.png 	new file:   public/img/icons/Skyrim.png 	new file:   public/img/icons/SwiftUI.png 	new file:   public/img/icons__iphone14/CSS3.png 	new file:   public/img/icons__iphone14/Html 5.png 	new file:   public/img/icons__iphone14/JavaScript.png 	new file:   public/img/icons__iphone14/React.png 	new file:   public/img/icons__iphone14/descarga__3_-removebg-preview 1.png 	new file:   public/img/icons__iphone14/git.png 	new file:   public/img/icons__iphone14/jquery.png 	new file:   public/img/icons__iphone14/kotlin.png 	new file:   public/img/icons__iphone14/node.js.png 	new file:   public/img/icons__iphone14/sql.png 	new file:   public/img/icons__iphone14/swift.png 	new file:   public/img/icons__iphone14/tailwind.png 	new file:   public/img/img__about/about__me.png 	new file:   public/img/img__header/GitHub.png 	new file:   public/img/img__header/Line 3.png 	new file:   public/img/img__header/Line 4.png 	new file:   public/img/img__header/arrows.png 	new file:   public/img/img__header/liquid.png 	new file:   public/img/img__project_1/img__project.png 	new file:   public/img/img__project_1/wave.png 	new file:   public/img/maquet__iphone14/button__iphone/bg__iphone.png 	new file:   public/img/maquet__iphone14/flags__iphone/alemania.png 	new file:   public/img/maquet__iphone14/flags__iphone/francia.png 	new file:   public/img/maquet__iphone14/flags__iphone/italia.png 	new file:   public/img/maquet__iphone14/flags__iphone/reino-unido.png 	new file:   public/img/maquet__iphone14/fond__iphone/font.jpg 	new file:   public/img/maquet__iphone14/fond__iphone/stat.png 	new file:   public/img/maquet__iphone14/phone_14.png 	new file:   public/img/stars/black_star.png 	new file:   public/img/stars/white__star.png 	new file:   public/img/waves__down/wave__100.png 	new file:   public/img/waves__down/wave__20.png 	new file:   public/img/waves__down/wave__50.png 	new file:   public/img/waves__up/wave__100.png 	new file:   public/img/waves__up/wave__20.png 	new file:   public/img/waves__up/wave__50.png 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/components/Header.jsx 	new file:   src/components/styles/querys/Headers/Headers1440.css 	new file:   src/components/styles/querys/Headers/Headers1512.css 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './components/Header'
+import Header from './components/Header'
+function App() {
+
+  return (
+    <body>
+      <Header></Header>
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import "./styles/querys/Headers/Headers1512.css";
+import "./styles/querys/Headers/Headers1440.css";
+
+
+const Header = () => {
+  return (
+    <main>
+      
+        <nav>
+          <h2 className="h2__Steven">Steven</h2>
+          <a className="h2__linkedin" target="blank" href="https://www.linkedin.com/in/bramdon-stiven-blanco-contreras-7182331b5/"><h2 >LN</h2></a>
+         <a className="h2__whats" href="https://w.app/BUHZlO" target="blank"><h2 >WH</h2></a> 
+        <a className="h2__github" href="https://github.com/Sendith" target="blank"><h2>GT</h2></a>  
+          <img className="line" src=".\img\img__header\Line 3.png" alt="" />
+          <h2 className="_dos">©/2023</h2>
+        </nav>
+
+        <div className="Apres">
+          <img className="liquid" src="\img\img__header\liquid.png" alt="" />
+          <h1 className="Hello">Hello.</h1>
+          <h3 className="h3__StevenBlanco">STEVEN BLANCO</h3><img className="git__header" src="\img\img__header\GitHub.png" alt="" />
+          <h4 className="h4__Looking">Looking for a job </h4>
+          <div className="circulo"></div>
+          <h4 className="Product">Product Designer / Webflow Developer / Framer Developer and Partner.</h4>
+          <h4 className="Current">Currently working full-time as a Senior Product Designer at <span className="Detail" >Detail Technologies</span> </h4> 
+
+        <img className="star1" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star2" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star3" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star4" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star5" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star6" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star7" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star8" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star9" src="\public\img\stars\black_star.png" alt="" />
+        <img className="star10" src="\public\img\stars\black_star.png" alt="" />
+
+
+
+
+        </div>
+      
+    </main>
+  )
+}
+
+export default Header
