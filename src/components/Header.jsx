3117ff930058cea06c956c18b0369b291ac07352
Stiven <delleore1@gmail.com>
2023-12-01 19:53:44 -0500

    modified:   src/components/Header.jsx 	modified:   src/components/styles/querys/Headers/HeadersQueryDesktop.css 	modified:   src/components/styles/querys/Headers/HeadersQueryTablet.css 	modified:   src/components/styles/querys/Headers/HeadersQueryiPhone.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
 import "./styles/querys/Headers/HeadersQueryDesktop.css";
 import "./styles/querys/Headers/HeadersQueryTablet.css";
 import "./styles/querys/Headers/HeadersQueryiPhone.css";
-
-
 const Header = () => {
   return (
     <main>
-      
         <nav>
           <h2 className="h2__Steven">Steven</h2>
           <a className="h2__linkedin" target="blank" href="https://www.linkedin.com/in/bramdon-stiven-blanco-contreras-7182331b5/"><h2 >LN</h2></a>
@@ -15,7 +12,6 @@ const Header = () => {
           <img className="line" src=".\img\img__header\Line 3.png" alt="" />
           <h2 className="_dos">©/2023</h2>
         </nav>
-
         <div className="Apres">
           <div className="flex__liquid">
           <img className="liquid" src="\img\img__header\liquid.png" alt="" />
@@ -28,7 +24,6 @@ const Header = () => {
           <div className="circulo"></div>
           <h4 className="Product">Product Designer / Webflow Developer / Framer Developer and Partner.</h4>
           <h4 className="Current">Currently working full-time as a Senior Product Designer at <span className="Detail" >Detail Technologies</span> </h4> 
-
         <img className="star1" src="\img\stars\black_star.png" alt="" />
         <img className="star2" src="\img\stars\black_star.png" alt="" />
         <img className="star3" src="\img\stars\black_star.png" alt="" />
@@ -39,17 +34,12 @@ const Header = () => {
         <img className="star8" src="\img\stars\black_star.png" alt="" />
         <img className="star9" src="\img\stars\black_star.png" alt="" />
         <img className="star10" src="\img\stars\black_star.png" alt="" />
-
     <div className="divsArrow">
     <img className="arrowUno" src="\img\img__header\arrows.png" alt="" />
         <img className="arrowDos" src="\img\img__header\arrows.png" alt="" />
         <img className="arrowTres" src="\img\img__header\arrows.png" alt="" />
     </div>
-       
-
-
         </div>
-      
     </main>
   )
 }
