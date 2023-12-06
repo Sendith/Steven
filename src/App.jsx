bf9ea8f8f19fb8506d8b0766d589cbdd43b969b0
Stiven <delleore1@gmail.com>
2023-12-06 16:36:01 -0500

    modified:   index.html 	modified:   public/img/img__about/about__me.png 	modified:   src/App.css 	modified:   src/App.jsx 	modified:   src/components/About.jsx 	new file:   src/components/styles/querys/Abouts.styles/AboutQueryDesktop.css 	new file:   src/components/styles/querys/Abouts.styles/AboutQueryIphone.css 	new file:   src/components/styles/querys/Abouts.styles/AboutQueryTablet.css 	new file:   src/fonts/Lauren.woff2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import './components/Header'
 import Header from './components/Header'
+import About from './components/About'
 function App() {
 
   return (
     <body>
       <Header></Header>
+      <About></About>
     </body>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,91 @@
+import "./styles/querys/Abouts.styles/AboutQueryDesktop.css";
+import "./styles/querys/Abouts.styles/AboutQueryTablet.css";
+import "./styles/querys/Abouts.styles/AboutQueryIphone.css";
+
+
+const About = () => {
+  return (
+
+    
+    <section className="Main_container">
+
+        <img className="star1" src="\img\stars\black_star.png" alt="" />
+        <img className="star2" src="\img\stars\black_star.png" alt="" />
+        <img className="star3" src="\img\stars\black_star.png" alt="" />
+        <img className="star4" src="\img\stars\black_star.png" alt="" />
+        <img className="star5" src="\img\stars\black_star.png" alt="" />
+        <img className="star6" src="\img\stars\black_star.png" alt="" />
+        <img className="star7" src="\img\stars\black_star.png" alt="" />
+        <img className="star8" src="\img\stars\black_star.png" alt="" />
+        <img className="star9" src="\img\stars\black_star.png" alt="" />
+        <img className="star10" src="\img\stars\black_star.png" alt="" />   
+
+
+        <div className="About">
+            <p className="">About.</p>
+        </div>
+        <div className="Steven">
+           <p>I m Steven. A designer, maker and problem solver.</p>
+        </div>
+        <div className="contenedorimg">
+    <img className="contenedor__img" src="\img\img__about\about__me.png" alt="" />
+        </div>
+        <div className="The_cusp">
+            <p>The cusp of art and technology has always fascinated me and I ve never been afraid to just jump in and give it a go, whether it s Paint, Photoshop, Sketch or CSS. I ve been designing with computers since the day I first opened Microsoft Paint.</p>
+        </div>
+        <div className="Fast_forward">
+            <p>Fast forward to 2023 and I’ve tried it all, from Digital Campaign Design and Flash Actionscript to Web Design, Animation, HTML/CSS, No-Code Web Development, Product Design and Product Management. Everything I have done, small or big, has been a vital stepping stone for where I am today.
+            </p>
+        </div>
+        <div className="Product_design">
+        <p>
+        What excites me most about being a Product Designer is being able to design and create things that have purpose and solve real problems. It goes beyond designing buttons and websites and involves having a passion for designing experiences and solutions that help people, whether its helping them make better videos, market themselves online, or buy something online. Leaning into customer insight and understanding their needs, finding the right problems to solve, delivering solutions as quickly as possible, learning from those and then iterating and improving that value over time is the key to great product design.
+        <br />
+        <br />
+        <span>This is one of my favourite quotes (by Peter Drucker):</span>  
+        </p>
+        </div>
+        <div className="There_is">
+            <p>
+            “There is nothing so useless as doing efficiently that which should not be done at all.”
+            </p>
+        </div>
+        <div className="This_resonates">
+            <p>This resonates so deeply with me because I have no desire to make pretty things just for the sake of it. Some people love that! But it s not for me. I only want to make, design or build ideas and products that are real, that matter and have an impact in the world.</p>
+        </div>
+        <h3 className="experience">Experience</h3>
+        <div className="degree">
+            <p>- Degree in Brand Communications
+            - 15 years experience in Digital Design, Web Development, Advertising and Product Development</p>
+        </div>
+        <div className="within">
+            <p>
+            Within those 15 years, I have:‍<br />
+            - 12 years experience in Web Design<br />
+            - 10 years experience in Software Design<br />
+            - 3 years experience in Front-end Web Development<br />
+            - 5 years experience in Webflow Development<br />
+            - 1 year experience in Framer Development<br />
+            - 4 years experience in Product Management<br />
+            </p>
+        </div>
+        <button className="Contact_Me">Contact Me</button>
+        <h3 className="skills">Skills</h3>
+        <div className="UI">
+            <p>
+            UI/UX Design / Strategy / Product Management / User Research / Agile Methodologies / Collaboration / Design Sprints / Design Systems / HTML and CSS / CMS Design & Architecture / Webflow Development / Framer Development / Photography / Graphic Design‍‍
+            </p>
+        </div>
+        <div className="Container_icons">
+            <img src="\img\icons\Android OS.png" alt="" />
+            <img src="\img\icons\JavaScript.png" alt="" />
+            <img src="\img\icons\Python.png" alt="" />
+            <img src="\img\icons\React.png" alt="" />
+            <img src="\img\icons\Skyrim.png" alt="" />
+            <img src="\img\icons\SwiftUI.png" alt="" />
+        </div>
+
+    </section>
+  )
+}
+export default About
