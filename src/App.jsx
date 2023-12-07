76eff85b883d68cf724b9749f726778eb4bd16f2
Stiven <delleore1@gmail.com>
2023-12-06 23:56:34 -0500

    modified:   src/App.jsx 	new file:   src/components/Tail.jsx 	new file:   src/components/styles/querys/Tail/TailDesktop.css 	new file:   src/components/styles/querys/Tail/TailIphone.css 	new file:   src/components/styles/querys/Tail/TailTablet.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import './components/Header'
 import Header from './components/Header'
 import About from './components/About'
+import Tail from './components/Tail'
 function App() {
 
   return (
     <body>
       <Header></Header>
       <About></About>
+      <Tail></Tail>
     </body>
   )
 }
---
+++ src/components/Tail.jsx
@@ -0,0 +1,39 @@
+import "./styles/querys/Tail/TailDesktop.css";
+
+const Tail = () => {
+  return (
+    <section className="Main">
+        <div className="Hi">
+          <p>Hi</p>
+        </div>
+        <div className="Parrafo">
+        <p>
+        Need a beautiful, well-structured website that you can own and maintain yoursel? Get in touch
+        <br />
+        <br />
+      Email; Delleore1@gmail.com
+    On the internet: Linkedin / GitHub / Whatsapp
+        </p>
+        </div>
+        <div className="cwaves">
+            <img className="wave1" src="\img\waves__down\wave__100.png" alt="" />
+            <img className="wave2" src="\img\waves__down\wave__50.png" alt="" />
+            <img className="wave3" src="\img\waves__down\wave__20.png" alt="" />
+            <div className="bloqueador">.</div>
+        </div>
+
+        <img className="star1" src="\img\stars\white__star.png" alt="" />
+        <img className="star2" src="\img\stars\white__star.png" alt="" />
+        <img className="star3" src="\img\stars\white__star.png" alt="" />
+        <img className="star4" src="\img\stars\white__star.png" alt="" />
+        <img className="star5" src="\img\stars\white__star.png" alt="" />
+        <img className="star6" src="\img\stars\white__star.png" alt="" />
+        <img className="star7" src="\img\stars\white__star.png" alt="" />
+        <img className="star8" src="\img\stars\white__star.png" alt="" />
+        <img className="star9" src="\img\stars\white__star.png" alt="" />
+        <img className="star10" src="\img\stars\white__star.png" alt="" />
+    </section>
+  )
+}
+
+export default Tail
